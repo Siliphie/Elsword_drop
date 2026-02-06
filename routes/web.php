5b79737e0b192850640806315cb2df66bb29915b
Damien <damien.langeraert@ynov.com>
2026-02-06 10:16:54 +0100

    create route for profile

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-Route::get('/login', function () {
-    return view('auth.login');
-})-> name('login');
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::post('/logout', function (){
-    return view('auth.logout');
-})->name('logout');
+
+Route::middleware('guest')->group(function () {
+Route::get('/login', function () {
+    return view('auth.login');
+})-> name('login');
 
 Route::get('/register', function(){
     return view('auth.register');
 })->name('register');
+});
 
-Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
-Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
