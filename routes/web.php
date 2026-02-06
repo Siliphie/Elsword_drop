9bb8fcbee1a4d8776b0692d65bab6a02a727306c
Damien <damien.langeraert@ynov.com>
2026-02-06 11:37:30 +0100

    add route for post login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ItemDropRateController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,13 +11,17 @@ Route::get('/', function () {
 
 
 Route::middleware('guest')->group(function () {
-Route::get('/login', function () {
-    return view('auth.login');
-})-> name('login');
+    // Affichage
+    Route::get('/login', function () {
+        return view('auth.login');
+    })->name('login');
 
-Route::get('/register', function(){
-    return view('auth.register');
-})->name('register');
+    Route::get('/register', function(){
+        return view('auth.register');
+    })->name('register');
+    
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
 
 
@@ -24,4 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/drops/create', [ItemDropRateController::class, 'create'])->name('drops.create');
+    Route::post('/drops', [ItemDropRateController::class, 'store'])->name('drops.store');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
